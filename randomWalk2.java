d81b1a1d1bd4d0c2aa67a606007aba3d9ac42a57
jsingleton128 <46461428+jsingleton128@users.noreply.github.com>
2019-02-06 14:17:41 -0800

    Add files via upload

--- randomWalk2.java
+++ randomWalk2.java
@@ -15,18 +15,15 @@ public class randomWalk2 {
 		while ((position > -3) && (position < 3)) { //loop until position is equal to 3 or -3
 			int coinFlip = rand.nextInt(3) + 1;		//50/50 chance to move forward or backward	
 				if (coinFlip == 1) {// move forward if coinFlip = 1 (add 1 to position)
-					if (position > max) {//if current position value is greater than max, reset max to new value
-						max = position;
-					}
 					position++;
 					System.out.println("position = " + position);
 					} else {// move backward if coinFlip = 2 (minus 1 to position)
-						if (position > max) {//if current position value is greater than max, reset max to new value
-							max = position;
-						}
-						position--;
-						System.out.println("position = " + position);
+					position--;
+					System.out.println("position = " + position);
 					}
+				if (position > max) {//if current position value is greater than max, reset max to new value
+					max = position;
+				}
 			}
 		System.out.println("max position = " + max);	
 		}
